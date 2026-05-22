5bfcafdfedbdbfd2479a8a6ba33e75f9b2ec7060
hydr0fx <zhydroflash@gmail.com>
2026-05-22 23:00:59 +0200

    Initial commit - ResellTracker App

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'reselltracker-v1';
+const URLS = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('Offline', { status: 503 })))
+  );
+});
