3f55f38723a16f5e1e83adbb9f0cc9c17db52213
hydr0fx <zhydroflash@gmail.com>
2026-05-23 00:49:36 +0200

    Network-first SW: immer neueste Version, kein Cache-Problem mehr

--- sw.js
+++ sw.js
@@ -1,35 +1,19 @@
-var CACHE = 'flippybird-v2.2.0';
-const URLS = [
-  '/resell-tracker/',
-  '/resell-tracker/index.html',
-  '/resell-tracker/manifest.json',
-  '/resell-tracker/icon-192.png',
-  '/resell-tracker/icon-512.png',
-  '/resell-tracker/apple-icon.png',
-  '/resell-tracker/splash-bird.png',
-  '/resell-tracker/sw.js'
-];
+var CACHE = 'flippybird-v2.3.0';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(URLS).catch(function(){})).then(function(){ return self.skipWaiting(); })
-  );
+  e.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(keys) { return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); })); }).then(function(){ return self.clients.claim(); })
+    caches.keys().then(function(keys) { return Promise.all(keys.map(function(k) { return caches.delete(k); })); }).then(function(){ return self.clients.claim(); })
   );
-});
-
-self.addEventListener('message', function(e) {
-  if (e.data && e.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(function(r) { return r; }).catch(function() {
+      return caches.match(e.request);
+    })
   );
 });
